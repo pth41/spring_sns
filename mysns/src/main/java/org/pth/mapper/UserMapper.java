c5392d5213fc7ebe206e3f5dc2a6add9bde14954
Hee <w@USERMIC-FG2VVUA>
2020-07-28 14:40:44 +0900

    UserMapper CRUD 작업 및 테스트

--- mysns/src/main/java/org/pth/mapper/UserMapper.java
+++ mysns/src/main/java/org/pth/mapper/UserMapper.java
@@ -1,5 +1,13 @@
 package org.pth.mapper;
 
-public interface UserMapper {
+import org.pth.domain.UserVO;
 
+public interface UserMapper {
+	public int insert(UserVO vo);
+	
+	public UserVO read(Long user_no);
+	
+	public int delete(Long user_no);
+	
+	public int update(UserVO vo);
 }
--- mysns/src/test/java/org/pth/mapper/UserMapperTests.java
+++ mysns/src/test/java/org/pth/mapper/UserMapperTests.java
@@ -1,7 +1,10 @@
 package org.pth.mapper;
 
+import java.util.stream.IntStream;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.pth.domain.UserVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -20,5 +23,48 @@ public class UserMapperTests {
 	@Test
 	public void testMapper() {
 		log.info(mapper);
+	}
+	
+	@Test
+	public void testCreate() {
+		IntStream.rangeClosed(1, 10).forEach(i -> {
+			UserVO vo = new UserVO();
+			
+			vo.setEmail("email-"+i);
+			vo.setPassword("password-"+i);
+			vo.setName("name-"+i);
+			vo.setUsername("username-"+i);
+			
+			mapper.insert(vo);
+		});
+	}
+	
+	@Test
+	public void testRead() {
+		Long tergetUser_no = 4L;
+		
+		UserVO vo = mapper.read(tergetUser_no);
+		
+		log.info(vo);
+	}
+	
+	@Test
+	public void testDelete() {
+		Long targetUser_no = 1L;
+		
+		mapper.delete(targetUser_no);
+	}
+	
+	@Test
+	public void testUpdate() {
+		Long targerUser_no = 10L;
+		
+		UserVO vo = mapper.read(targerUser_no);
+		
+		vo.setName("Update Name ");
+		
+		int count = mapper.update(vo);
+		
+		log.info("UPDATE COUNT: "+count);
 	}
 }
