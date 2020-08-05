e6cea3312d34bfdf2e014eb763193216a84f3a10
Hee <w@USERMIC-FG2VVUA>
2020-08-05 14:15:23 +0900

    LoginSuccessHandler 작성

---
+++ mysns/src/main/java/org/pth/security/CustomLoginSuccessHandler.java
@@ -0,0 +1,40 @@
+package org.pth.security;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import lombok.extern.log4j.Log4j;
+
+@Log4j
+public class CustomLoginSuccessHandler implements AuthenticationSuccessHandler {
+	
+	@Override
+	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication auth) 
+			throws IOException, ServletException {
+		log.warn("Login Success");
+		
+		List<String> roleNames = new ArrayList<>();
+		
+		auth.getAuthorities().forEach(authority -> {
+			roleNames.add(authority.getAuthority());
+		});
+		
+		log.warn("ROLE NAMES: " + roleNames);
+		
+		//ROLE_USER 권한을 가질 시 로그인 후 /users 로 이동
+		if(roleNames.contains("ROLE_USER")) {
+			response.sendRedirect("/users");
+			return;
+		}
+		
+		response.sendRedirect("/");
+	}
+}
