33381dea43cbd9e9c63ccc93202d7b04b1aa0998
Hee <w@USERMIC-FG2VVUA>
2020-08-19 11:46:56 +0900

    ajax 모듈 추가
    reply.js 댓글 모듈 / user.js 회원 모듈

---
+++ mysns/src/main/webapp/resources/js/user.js
@@ -0,0 +1,106 @@
+console.log("User Module........");
+var userService = (function(){
+	function add(user, callback, error){
+		console.log("add user..............");
+		
+		$.ajax({
+			type : 'post',
+			url : '/users/new',
+			data : JSON.stringify(user),
+			contentType : "application/json; charset=utf-8",
+			success : function(result, status, xhr) {
+				if(callback){
+					callback(result);
+				}
+			},
+			error : function(xhr, status, er) {
+				if(error) {
+					error(er);
+				}
+			}
+		})
+	}
+	
+	function remove(user_no, callback, error) {
+		$.ajax({
+			type : 'delete',
+			url : '/users/'+user_no,
+			success : function(deleteResult, status, xhr) {
+				if(callback) {
+					callback(deleteResult);
+				}
+			},
+			error : function(xhr, status, er) {
+				if(error) {
+					error(er);
+				}
+			}
+		});
+	}
+	
+	function update(user, callback, error) {
+		console.log("user_no: "+user.user_no);
+		
+		$.ajax({
+			type : 'put',
+			url : '/users/'+user.user_no,
+			data : JSON.stringify(user),
+			contentType : "application/json; charset=utf-8",
+			success : function(result, status, xhr) {
+				if(callback) {
+					callback(result);
+				}
+			},
+			error : function(xhr, status, er) {
+				if(error) {
+					error(er);
+				}
+			}
+		});
+	}
+	
+	function get(user_no, callback, error) {
+		$.get("/users/"+user_no+".json", function(result) {
+			if(callback) {
+				callback(result);
+			}
+		}).fail(function(xhr, status, err) {
+			if(error) {
+				error();
+			}
+		});
+	}
+	
+	function displayTime(timeValue) {
+		var today = new Date();
+		
+		var gap = today.getTime() - timeValue;
+		
+		var dateObj = new Date(timeValue);
+		var str = "";
+		
+		if(gap < (1000 * 60 * 60 * 24)) {
+			var hh = dateObj.getHours();
+			var mi = dateObj.getMinutes();
+			var ss = dateObj.getSeconds();
+			
+			return [ (hh > 9 ? '' : '0') + hh, ':', (mi > 9 ? '' : '0') + mi, ':', (ss > 9 ? '' : '0') + ss ].join('');
+		} else {
+			var yy = dateObj.getFullYear();
+			var mm = dateObj.getMonth() + 1; // getMonth() is zero-based
+			var dd = dateObj.getDate();
+			
+			return [ yy, '/', (mm > 9 ? '' : '0') + mm, '/', (dd > 9 ? '' : '0') + dd ].join('');
+		}
+	};
+	
+	return {
+		add : add,
+		get : get,
+		remove : remove,
+		update : update,
+		displayTime : displayTime
+		
+	};
+	
+})();
