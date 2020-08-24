552679e294b7160d3936df6d15dca0b64665c58c
Hee <w@USERMIC-FG2VVUA>
2020-08-24 10:40:38 +0900

    AuthVO 객체를 받아 insertAuth 실행

--- mysns/src/main/java/org/pth/mapper/UserMapper.java
+++ mysns/src/main/java/org/pth/mapper/UserMapper.java
@@ -1,11 +1,12 @@
 package org.pth.mapper;
 
+import org.pth.domain.AuthVO;
 import org.pth.domain.UserVO;
 
 public interface UserMapper {
 	public int insert(UserVO vo);
 	
-	public int insertAuth(String email, String auth);
+	public int insertAuth(AuthVO vo);
 	
 	public UserVO read(Long user_no);
 	
