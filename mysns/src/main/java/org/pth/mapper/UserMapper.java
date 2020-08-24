fa4cbdc124fb0911ef4444b57b0983acacb3aa23
Hee <w@USERMIC-FG2VVUA>
2020-08-24 10:15:56 +0900

    insertAuth 추가
    권한 부여 맵퍼

--- mysns/src/main/java/org/pth/mapper/UserMapper.java
+++ mysns/src/main/java/org/pth/mapper/UserMapper.java
@@ -5,6 +5,8 @@ import org.pth.domain.UserVO;
 public interface UserMapper {
 	public int insert(UserVO vo);
 	
+	public int insertAuth(String email, String auth);
+	
 	public UserVO read(Long user_no);
 	
 	public UserVO readByEmail(String email);
