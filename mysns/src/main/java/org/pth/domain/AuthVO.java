80f3aaa1e0bf58f0673d3ba042bd8ba3d752881c
Hee <w@USERMIC-FG2VVUA>
2020-08-24 10:39:33 +0900

    생성자 추가

--- mysns/src/main/java/org/pth/domain/AuthVO.java
+++ mysns/src/main/java/org/pth/domain/AuthVO.java
@@ -6,4 +6,11 @@ import lombok.Data;
 public class AuthVO {
 	private String email;
 	private String auth;
+	
+	public AuthVO(String email, String auth) {
+		this.email = email;
+		this.auth = auth;
+	}
+	
+	public AuthVO() {}
 }
