30c371c38805287a9df797a7e44e76a77bf6290c
Hee <Hee@192.168.1.107>
2020-08-27 18:38:12 +0900

    ReplyController 생성
    CRUD getList

---
+++ mysns/src/main/java/org/pth/controller/ReplyController.java
@@ -0,0 +1,89 @@
+package org.pth.controller;
+
+import java.util.List;
+
+import org.pth.domain.ReplyVO;
+import org.pth.service.ReplyService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.AllArgsConstructor;
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+
+@RequestMapping("/replies/")
+@RestController
+@Log4j
+@AllArgsConstructor
+public class ReplyController {
+	
+	@Setter(onMethod_ = @Autowired)
+	private ReplyService service;
+
+	@GetMapping(value = "/list/{post_no}",
+			produces = { MediaType.APPLICATION_XML_VALUE,
+						 MediaType.APPLICATION_JSON_UTF8_VALUE })
+	public ResponseEntity<List<ReplyVO>> getList(@PathVariable("post_no") Long post_no) {
+		log.info("reply_getList");
+		
+		return new ResponseEntity<>(service.getList(post_no), HttpStatus.OK);
+	}
+	
+	@PostMapping(value = "/new",
+			consumes = "application/json",
+			produces = { MediaType.TEXT_PLAIN_VALUE })
+	public ResponseEntity<String> create(@RequestBody ReplyVO vo) {
+		log.info("ReplyVO: "+vo);
+		
+		int insertCount = service.register(vo);
+		
+		log.info("Reply INSERT COUNT: "+insertCount);
+		
+		return insertCount == 1
+				? new ResponseEntity<>("success", HttpStatus.OK)
+				: new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+	
+	@GetMapping(value = "/{reply_no}",
+			produces = { MediaType.APPLICATION_XML_VALUE,
+						 MediaType.APPLICATION_JSON_UTF8_VALUE })
+	public ResponseEntity<ReplyVO> get(@PathVariable("reply_no") Long reply_no) {
+		log.info("reply_get: "+reply_no);
+		
+		return new ResponseEntity<>(service.get(reply_no), HttpStatus.OK);
+	}
+	
+	@DeleteMapping(value = "/{reply_no}",
+			produces = { MediaType.TEXT_PLAIN_VALUE })
+	public ResponseEntity<String> remove(@PathVariable("reply_no") Long reply_no) {
+		log.info("reply_remove: "+reply_no);
+		
+		return service.remove(reply_no) == 1
+				? new ResponseEntity<>("success", HttpStatus.OK)
+				: new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+	
+	@RequestMapping(method = { RequestMethod.PUT, RequestMethod.PATCH },
+			value = "/{post_no}",
+			consumes = "application/json",
+			produces = { MediaType.TEXT_PLAIN_VALUE })
+	public ResponseEntity<String> modify(@RequestBody ReplyVO vo, @PathVariable("reply_no") Long reply_no) {
+		vo.setUser_no(reply_no);
+		log.info("reply_no: "+reply_no);
+		log.info("reply_modify: "+vo);
+		
+		return service.modify(vo) == 1
+				? new ResponseEntity<>("success", HttpStatus.OK)
+				: new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+}
