4fda89e3d1630cd540079abd58b0c4a1938d5103
Hee <Hee@192.168.1.107>
2020-08-27 18:25:22 +0900

    ReplyService 인터페이스 작성

---
+++ mysns/src/main/java/org/pth/service/ReplyService.java
@@ -0,0 +1,17 @@
+package org.pth.service;
+
+import java.util.List;
+
+import org.pth.domain.ReplyVO;
+
+public interface ReplyService {
+	public int register(ReplyVO vo);
+	
+	public ReplyVO get(Long reply_no);
+	
+	public int modify(ReplyVO vo);
+	
+	public int remove(Long reply_no);
+	
+	public List<ReplyVO> getList(Long post_no);
+}
