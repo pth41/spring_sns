6cb99aa6fefcc8e0b9f36297466b6c119eaa5ab6
Hee <Hee@192.168.1.107>
2020-09-15 16:00:45 +0900

    이미지 데이터 전송 코드

--- mysns/src/main/java/org/pth/controller/UploadController.java
+++ mysns/src/main/java/org/pth/controller/UploadController.java
@@ -11,10 +11,13 @@ import java.util.List;
 import java.util.UUID;
 
 import org.pth.domain.PostAttachVO;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
@@ -97,5 +100,27 @@ public class UploadController {
 			} // end try
 		} // end for
 		return new ResponseEntity<>(list, HttpStatus.OK);
+	}
+	
+	@GetMapping("/display")
+	@ResponseBody
+	public ResponseEntity<byte[]> getFile(String fileName) {
+		log.info("fileName: "+fileName);
+		
+		File file = new File("c:\\upload\\"+fileName);
+		
+		log.info("file: "+file);
+		
+		ResponseEntity<byte[]> result = null;
+		
+		try {
+			HttpHeaders header = new HttpHeaders();
+			
+			header.add("Content-Type", Files.probeContentType(file.toPath()));
+			result = new ResponseEntity<>(FileCopyUtils.copyToByteArray(file), header, HttpStatus.OK);
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
+		return result;
 	}
 }
